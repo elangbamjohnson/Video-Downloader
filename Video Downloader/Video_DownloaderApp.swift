fae8bee0bc578e47a68203342e2f85964150a928
Johnson <elangbamj@avaya.com>
2026-04-17 11:42:35 +0530

    Initial commit

---
+++ Video Downloader/Video_DownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video_DownloaderApp.swift
+//  Video Downloader
+//
+//  Created by Johnson on 17/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Video_DownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
